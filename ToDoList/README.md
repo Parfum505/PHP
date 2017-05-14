01f171d3ada64a928009b1197d130ce69c403e1b
Pavel <parfum505@gmail.com>
2017-05-14 13:07:57 +0200

    creating a project structure

---
+++ ToDoList/README.md
@@ -0,0 +1,3 @@
+# To-Do List with PHP, MySQL and JQuery
+
+Here is my first project on PHP, traditionally it is To-Do list. 
---
+++ ToDoList/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ToDo App</title>
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/lib/jquery-1.11.0.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<h1>To-Do List <span class="title">with PHP</span></h1>
+		<div class="form">
+			<form action="">
+				<input type="text" name="text">
+			</form>
+		</div>
+		<ul>
+			<li>Learn PHP</li>
+			<li>Learn JS</li>
+			<li>Learn CSS</li>
+		</ul>
+	</div>
+	<script src="JS/script.js"></script>
+</body>
+
+</html>
