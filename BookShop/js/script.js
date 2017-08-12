591c73d8f0e0af38d2547f93d93fbeb65fa23b83
Pavel <parfum505@gmail.com>
2017-08-12 13:41:58 +0200

    add js to nav_main

--- BookShop/js/script.js
+++ BookShop/js/script.js
@@ -3,18 +3,22 @@ function slideToggle(animationClass){
 	var content = this.parentNode.querySelector('.content');
 	content.classList.toggle(animationClass);
 }
-
+/* Open/close class 'content' in terms_conditions */
 document.addEventListener('click', function (e) {
 	if (e.target.className == 'slideToggle') {
 		e.preventDefault();
 		slideToggle.call(e.target, 'show');
 	}
 });
+/* Open/close nav_main in mobile version */
 document.querySelector('.nav_btn_mobile').addEventListener('click', function (e) {
 	slideToggle.call(this,'show');
-
 });
-
+/* Switch class 'active' in nav_main */
+document.querySelector('.nav_main').addEventListener('click', function (e) {
+	document.querySelector('.nav_main .active').classList.remove('active');
+	e.target.classList.add('active');
+});
 
 
 
