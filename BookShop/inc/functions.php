c2114bd052ce12b36432b7676f7bcb19b456c612
Pavel <parfum505@gmail.com>
2017-09-24 18:37:22 +0200

    add get_products func

--- BookShop/inc/functions.php
+++ BookShop/inc/functions.php
@@ -12,5 +12,22 @@ function show_categories(){
 		echo "<li><a href='catalog.php?id=".$row['cat_id']. "'>".$row['cat_title']."</a></li>";
 	}
 }
+function get_products(){
+	global $link;
+	$query = "SELECT * FROM products";
+	$res = mysqli_query($link, $query);
+	if (!$res) {
+		die("Error: " . mysqli_error($link));
+	}
+	while ($row = mysqli_fetch_assoc($res)) {
+		echo "<a class='product' href='catalog.php?id=".$row['prod_id']."'>";
+		echo "<img src='".$row['prod_img']."' alt='".$row['prod_title']."'>";
+		echo "<p>".$row['prod_title']."</p>";
+		echo "<p>".$row['prod_author']."</p>";
+		echo "<p>".$row['prod_description']."</p>";
+		echo "<p>".$row['prod_price']."</p>";
+		echo "</a>";
+	}
+}
 
 /*** Admin functions ***/
--- BookShop/pages/catalog.php
+++ BookShop/pages/catalog.php
@@ -1,11 +1,5 @@
-<?php
+<?php require "../inc/config.php"; ?>
 
-require "../inc/config.php";
-
-
-	//$goods = selectAllItems();
-
-?>
 <?php $title = "BookShop catalogue"; ?>
 <?php include_once (COMMON_PAGES . "header.php"); ?>
 
@@ -13,27 +7,9 @@ require "../inc/config.php";
 	<h1>Bookshop</h1>
 	<div class="row">
 <?php include_once (COMMON_PAGES . "aside_shop.php"); ?>
-
-<p>Items in <a href="basket.php">the cart</a>: <?= $count ?></p>
-<table border="1" cellpadding="5" cellspacing="0" width="100%">
-<tr>
-	<th>Title</th>
-	<th>Author</th>
-	<th>Year of publish</th>
-	<th>Price, euro</th>
-	<th>Add to cart</th>
-</tr>
-
-<?php foreach($goods as $item): ?>
-	<tr>
-		<td><?= $item["title"] ?></td>
-		<td><?= $item["author"] ?></td>
-		<td><?= $item["pubyear"] ?></td>
-		<td><?= $item["price"] ?></td>
-		<td><a href="add2basket.php?id=<?= $item['id']?>">Add to cart</a></td>
-	</tr>
-<?php endforeach;?>
-</table>
+<main>
+	<?php get_products(); ?>
+</main>
 	</div>
 	<!-- /.row -->
 </div>
