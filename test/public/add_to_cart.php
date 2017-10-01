2c39aeb1ea82f1af3c29373677df0b8ac1f1c967
Pavel <parfum505@gmail.com>
2017-10-01 13:14:04 +0200

    change add2cart.php

--- test/public/add_to_cart.php
+++ test/public/add_to_cart.php
@@ -4,8 +4,8 @@
 if (isset($_POST['add'])) {
 	$id= (int)$_POST['item_id'];
 	$qu= (int)$_POST['quantity'];
-	$lemon= isset($_POST['lemon'])?$_POST['lemon'] : '' ;
-	$ice= isset($_POST['ice'])?$_POST['ice'] : '' ;
+	$lemon= isset($_POST['lemon'])? trim(strip_tags($_POST['lemon'])) : '' ;
+	$ice= isset($_POST['ice'])? trim(strip_tags($_POST['ice'])) : '' ;
 	$ref = $_SERVER["HTTP_REFERER"];
 	add2Cart($id, $qu, $lemon, $ice);
 
